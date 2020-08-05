568f2f4b113a1895bc0b50abd8a0d8f927627436
VinaTsai <vinatsai2325@gmail.com>
2020-08-06 02:51:13 +0800

    前台部屬

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    publicPath: './',
+    pwa: {
+      manifestOptions: {
+        name: 'Light Up Old House',
+        short_name: 'oldHouseLighting',
+        theme_color: '#4DBA87',
+        background_color: '#000',
+        scope: './',
+        start_url: './',
+        display: 'standalone'
+      }
+    }
+  }
